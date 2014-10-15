41d06ccf1706c2fc2200eeb098ed2df108ace455
muellermike <miki.mueller@hotmail.com>
2014-10-15 21:23:06 +0200

    added GalleryRepository

---
+++ CompetenceAppM152.DataAccessLayer/Repositories/GalleryRepository.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetenceAppM152.DataAccessLayer.Repositories
+{
+    public class GalleryRepository
+    {
+        public GalleryRepository()
+        {
+            string connectionString = "server=localhost;uid=root;pwd=;database=gallery;";
+            try
+            {
+                MySqlConnection conn = new MySqlConnection();
+                conn.ConnectionString = connectionString;
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM album";
+                cmd.Prepare();
+                MySqlDataReader reader = cmd.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
---
+++ CompetenceAppM152/Controllers/GalleryController.cs
@@ -0,0 +1,19 @@
+﻿using CompetenceAppM152.DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompetenceAppM152.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            GalleryRepository repo = new GalleryRepository();
+            return View();
+        }
+    }
+}
