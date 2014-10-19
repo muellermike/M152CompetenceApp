2a667acec83ca4d8f25c4213c4fb25e38e35fcfe
muellermike <miki.mueller@hotmail.com>
2014-10-19 17:25:24 +0200

    sync common project

---
+++ CompetenceAppM152.Common/Models/Gallery.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetenceAppM152.Common.Models
+{
+    public class Gallery
+    {
+        private Guid _identifier;
+        private string _name;
+        private string _description;
+        private Guid _titlepictureIdentifier;
+        private Picture _titlePicture = null;
+        private List<Picture> _pictures = null;
+
+        public Gallery()
+        {
+            _identifier = Guid.NewGuid();
+        }
+
+        public Gallery(Guid id, string name, string desc, Picture pic)
+        {
+            _identifier = id;
+            _name = name;
+            _description = desc;
+            _titlePicture = pic;
+            _titlepictureIdentifier = pic.Identifier;
+        }
+
+        public Gallery(Guid id, string name, string desc)
+        {
+            _identifier = id;
+            _name = name;
+            _description = desc;
+        }
+
+        public Guid Identifier
+        {
+            get { return _identifier; }
+            set { _identifier = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public Guid TitlePictureIdentifier
+        {
+            get { return _titlepictureIdentifier; }
+        }
+
+        public Picture TitlePicture
+        {
+            get { return _titlePicture; }
+            set { _titlePicture = value; }
+        }
+
+        public List<Picture> Pictures
+        {
+            get { return _pictures; }
+            set { _pictures = value; }
+        }
+    }
+}
---
+++ CompetenceAppM152.Common/Models/Picture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetenceAppM152.Common.Models
+{
+    public class Picture
+    {
+        private Guid _identifier;
+        private string _title;
+        private string _description;
+        private string _path;
+        private Guid _galleryFK;
+        private Gallery _gallery;
+
+        public Picture(Guid id, string title, string description, string path, Gallery gallery)
+        {
+            _identifier = id;
+            _title = title;
+            _description = description;
+            _path = path;
+            _gallery = gallery;
+            _galleryFK = gallery.Identifier;
+        }
+
+        public Picture(Guid id, string path)
+        {
+            _identifier = id;
+            _path = path;
+        }
+
+        public Guid Identifier
+        {
+            get { return _identifier; }
+            set { _identifier = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
+        }
+
+        public Guid GalleryFK
+        {
+            get { return _galleryFK; }
+        }
+
+        public Gallery Gallery
+        {
+            get { return _gallery; }
+            set { _gallery = value; }
+        }
+    }
+}
